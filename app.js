f9ece5fe9bd77c274e749a1948eb300029e5002a
Ihagatiana <iramilijaona@ingenosya.mg>
2024-01-31 15:12:43 +0300

    [REM] Time out

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ app.use("/auth", (req, res, next) => {
 });
 
 app.use("/appointment", async (req, res, next) => {
-  setTimeout(() => {
-    const appointmentModule = require("./src/appointment");
-    const router = appointmentModule.controller;
-    router(req, res, next);
-  }, 10000);
+  const appointmentModule = require("./src/appointment");
+  const router = appointmentModule.controller;
+  router(req, res, next);
 });
 
 app.listen(3000, () => {
