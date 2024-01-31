8a4a8b203a946cc7e1129a1c6ee26ba3635284d1
Ihagatiana <iramilijaona@ingenosya.mg>
2024-01-31 14:42:29 +0300

    [ADD] Test service with export

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 var app = express();
 const authModule = require("./src/auth");
-
-app.use("/auth", authModule.controller);
-
-
+const authService = authModule.service;
+app.use("/auth", authModule.router);
+authService.test();
 app.listen(3000, () => {
   console.log("Server is started on port 3000");
 });
--- src/auth/index.js
+++ src/auth/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const service = require("./auth.service");
 const authController = require("./auth.controller");
+const authService = require("./auth.service");
 router.use(authController);
 
-exports.controller = router;
-exports.service = service;
+module.exports = { router, service: authService };
