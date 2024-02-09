b7981612195a55dc97b7e8f41ee92f74b1e4fdbb
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-09 16:12:07 +0300

    modif env

--- src/service/service.service.js
+++ src/service/service.service.js
@@ -1,9 +1,7 @@
 const Service = require('./service.model');
   const getAllServices =  async() => {
     try {
-      console.log('Before Service.find()');
       const services = await Service.find({});
-      console.log('After Service.find():', services);
       return services;
     } catch (err) {
       throw new Error('Erreur lors de la récupération des services : ' + err.message);
