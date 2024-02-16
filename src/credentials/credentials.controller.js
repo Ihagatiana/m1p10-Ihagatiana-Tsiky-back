90319bfc9d9293c92feee69f8ac2d8d2d7184ad9
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-16 09:55:27 +0300

    [fix] credentials

--- src/credentials/credentials.controller.js
+++ src/credentials/credentials.controller.js
@@ -23,6 +23,24 @@ router.get("/:id",async (req, res) => {
 });
 
 
+router.post('/', async (req, res) => {
+    try {
+      const newCredentials = await credentialsService.create(req.body);
+      res.status(200).send(newCredentials);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+      const updatedCredentials = await credentialsService.updateById(req.params.id, req.body);
+      res.status(200).send(updatedCredentials);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
 
 router.delete("/:id", async (req, res) => {
     const credentialsId = req.params.id;
--- src/credentials/credentials.service.js
+++ src/credentials/credentials.service.js
@@ -1,33 +1,58 @@
 const Credentials = require('./credentials.model');
 
-  const findAll =  async() => {
-    try {
-      const credentials = await Credentials.find()
-      return credentials;
-    } catch (error) {   
-      throw new Error('Erreur finding credentials : ',error.message);
+    const findAll =  async() => {
+        try {
+        const credentials = await Credentials.find()
+        return credentials;
+        } catch (error) {   
+        throw new Error('Erreur finding credentials : ',error.message);
+        }
     }
-  }
 
-  const findById = async (credentialsId) => {
-    try {
-      const credentials = await Credentials.findById(credentialsId)
-      return credentials;
-    } catch (error) {
-      throw new Error('Error finding credentials : ',error);
-    }
-  };
+    const findById = async (credentialsId) => {
+        try {
+        const credentials = await Credentials.findById(credentialsId)
+        return credentials;
+        } catch (error) {
+        throw new Error('Error finding credentials : ',error);
+        }
+    };
 
-  const deleteById = async () => {
-    try {
-      const deletedCredentials = await Credentials.deleteOne({ _id: credentialsId });
-      if (deletedCredentials.deletedCount === 0) {
-        throw new Error('Credentials not found');
+    const create = async (credentialsData) => {
+        try {
+          const newCredentials = new Credentials(credentialsData);
+          const savedCredentials = await newCredentials.save();
+          return savedCredentials;
+        } catch (error) {
+          throw error;
+        }
       }
-      return { message: 'Credentials deleted successfully' };
-    } catch (error) {
-      return error;
-    }
-  };
 
-  module.exports = {findAll,findById,deleteById};
+    const updateById = async (credentialsId, updatedData)=> {
+        try {
+        const existingCredentials = await Credentials.findById(credentialsId);
+        Object.keys(updatedData).forEach(key => {
+            if (updatedData[key] !== undefined) {
+            existingCredentials[key] = updatedData[key];
+            }
+        });
+        const updatedCredentials = await existingCredentials.save();
+        return updatedCredentials;
+        } catch (error) {
+        throw error;
+        }
+    }
+  
+    const deleteById = async () => {
+        try {
+        const deletedCredentials = await Credentials.deleteOne({ _id: credentialsId });
+        if (deletedCredentials.deletedCount === 0) {
+            throw new Error('Credentials not found');
+        }
+        return { message: 'Credentials deleted successfully' };
+        } catch (error) {
+        return error;
+        }
+    };
+
+  module.exports = {findAll,findById,create,updateById,deleteById};
