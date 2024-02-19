293cd735a3ab8257c03b3fb50fad4d3a5402cf36
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-19 17:08:16 +0300

    [FIX] Service get All method to take pagination

--- src/services/services.controller.js
+++ src/services/services.controller.js
@@ -1,73 +1,92 @@
-  const express = require("express");
-  const router = express.Router();
-  const servicesService = require('./services.service');
-  const multer = require('multer');
+const express = require("express");
+const router = express.Router();
+const servicesService = require("./services.service");
+const multer = require("multer");
 
-  const storage = multer.memoryStorage();
-  const upload = multer({ storage: storage }); 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-  router.get("/",async (req, res) => {
-      try {
-        const services = await servicesService.findAll({});
-        res.status(200).json(services);
-      } catch (err) {
-        res.status(500).json({ message: err.message });
-      }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const services = await servicesService.findAll({
+      limit: req.query?.limit,
+      offset: req.query?.offset,
+    });
+    res.status(200).json(services);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-  router.get("/:id",async (req, res) => {
-    const servicesId = req.params.id;
-    try {
-      const services =  await servicesService.findById({ _id: servicesId });
-      res.status(200).json(services);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
+router.get("/:id", async (req, res) => {
+  const servicesId = req.params.id;
+  try {
+    const services = await servicesService.findById({ _id: servicesId });
+    res.status(200).json(services);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-  router.post('/', upload.array('photos'), async (req, res) => {
-    try {
-      const { name, price, duration, description } = req.body;
-      const imageBuffers = req.files ? req.files.map(file => file.buffer) : null;
-      const files = req.files; 
-      const newService = await servicesService.create(name, price, duration, description, imageBuffers,files);
-      res.status(201).send(newService);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(error.message);
-    }
-  });
+router.post("/", upload.array("photos"), async (req, res) => {
+  try {
+    const { name, price, duration, description } = req.body;
+    const imageBuffers = req.files
+      ? req.files.map((file) => file.buffer)
+      : null;
+    const files = req.files;
+    const newService = await servicesService.create(
+      name,
+      price,
+      duration,
+      description,
+      imageBuffers,
+      files
+    );
+    res.status(201).send(newService);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
 
-  router.put("/:id", upload.array('photos'), async (req, res) => {
-    try {
-      const serviceId = req.params.id;
-      const imagesBuffers = req.files ? req.files.map(file => file.buffer) : null; 
-      const otherServiceData = {
-        name: req.body.name,
-        price: req.body.price, 
-        duration: req.body.duration,
-        description: req.body.description
-      };
-      const file = req.files;
-      const updatedService =  await servicesService.updateById(serviceId, imagesBuffers, otherServiceData, file);
-      res.status(200).send(updatedService);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-  router.delete("/:id", async (req, res) => {
+router.put("/:id", upload.array("photos"), async (req, res) => {
+  try {
     const serviceId = req.params.id;
-    try {
-      const result = await servicesService.deleteById(serviceId);
-      if (result.error) {
-        return res.status(500).json({ message: 'Internal Server Error' });
-      }
-      res.status(200).json({ message: 'Service deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
+    const imagesBuffers = req.files
+      ? req.files.map((file) => file.buffer)
+      : null;
+    const otherServiceData = {
+      name: req.body.name,
+      price: req.body.price,
+      duration: req.body.duration,
+      description: req.body.description,
+    };
+    const file = req.files;
+    const updatedService = await servicesService.updateById(
+      serviceId,
+      imagesBuffers,
+      otherServiceData,
+      file
+    );
+    res.status(200).send(updatedService);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
-  module.exports = router;
+router.delete("/:id", async (req, res) => {
+  const serviceId = req.params.id;
+  try {
+    const result = await servicesService.deleteById(serviceId);
+    if (result.error) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    res.status(200).json({ message: "Service deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- src/services/services.service.js
+++ src/services/services.service.js
@@ -1,89 +1,110 @@
-const Services = require('./services.model');
-const imagesService = require('../util/images/images.services');
+const Services = require("./services.model");
+const imagesService = require("../util/images/images.services");
 
-const fs = require('fs');
-const { json } = require('express');
+const fs = require("fs");
+const { json } = require("express");
 
-  const findAll =  async() => {
-    try {
-      const services = await Services.find().populate('images');
-      return services;
-    } catch (error) {
-      throw new Error('Erreur finding services : ',error.message);
-    }
+const findAll = async ({ limit, offset }) => {
+  try {
+    const services = await Services.find()
+      .populate("images")
+      .skip(offset)
+      .limit(limit);
+    const total = await Services.count();
+    return { total, data: services };
+  } catch (error) {
+    throw new Error("Erreur finding services : ", error.message);
   }
+};
 
-  const findById = async (servicesId) => {
-    
-    try {
-      const services = await Services.findById(servicesId).populate('images');
-      return services;
-    } catch (error) {
-      throw new Error('Error finding services : ',error);
-    }
-  };
-  
-  const create = async (name, price, duration, description, imagesBuffers,file) => {
-    try {
-      const existingService = await Services.findOne({ name });
-      if (existingService) {
-        throw new Error('Service name already exists.');
-      }
-      const durationParsed = JSON.parse(duration)
-      const images = await imagesService.saveImageToFolderAndDatabase(imagesBuffers, file);
-      const service = new Services({ name, price, duration: durationParsed, description,images});
-      await service.save();
-      return service;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  const updateById =async (serviceId, imagesBuffers, otherServiceData, file) => {
-    try {
-
-      const existingService = await Services.findById(serviceId);
-      if (!existingService) {
-        throw new Error('Service not found');
-      }
-      if (otherServiceData.name) {
-        existingService.name = otherServiceData.name;
-      }
-      if (otherServiceData.price) {
-        existingService.price = otherServiceData.price;
-      }
-      if (otherServiceData.duration) {
-        existingService.duration = otherServiceData.duration;
-      }
-      if (otherServiceData.description) {
-        existingService.description = otherServiceData.description;
-      }
-      if (otherServiceData.description) {
-        existingService.description = otherServiceData.description;
-      }
-      if (imagesBuffers != null){
-        const images = await imagesService.saveImageToFolderAndDatabase(imagesBuffers, file);
-        existingService.images = images;
-      }
-
-      await existingService.save();
-      return existingService;
-    } catch (error) {
-      throw error;
-    }
+const findById = async (servicesId) => {
+  try {
+    const services = await Services.findById(servicesId).populate("images");
+    return services;
+  } catch (error) {
+    throw new Error("Error finding services : ", error);
   }
+};
 
-  const deleteById = async (serviceId) => {
-    try {
-      const deletedService = await Services.deleteOne({ _id: serviceId });
-
-      if (deletedService.deletedCount === 0) {
-        throw new Error('Service not found');
-      }
-      return { message: 'Service deleted successfully' };
-    } catch (error) {
-      return error;
+const create = async (
+  name,
+  price,
+  duration,
+  description,
+  imagesBuffers,
+  file
+) => {
+  try {
+    const existingService = await Services.findOne({ name });
+    if (existingService) {
+      throw new Error("Service name already exists.");
     }
-  };
+    const durationParsed = JSON.parse(duration);
+    const images = await imagesService.saveImageToFolderAndDatabase(
+      imagesBuffers,
+      file
+    );
+    const service = new Services({
+      name,
+      price,
+      duration: durationParsed,
+      description,
+      images,
+    });
+    await service.save();
+    return service;
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports = {findAll,findById,create,updateById,deleteById};
+const updateById = async (serviceId, imagesBuffers, otherServiceData, file) => {
+  try {
+    const existingService = await Services.findById(serviceId);
+    if (!existingService) {
+      throw new Error("Service not found");
+    }
+    if (otherServiceData.name) {
+      existingService.name = otherServiceData.name;
+    }
+    if (otherServiceData.price) {
+      existingService.price = otherServiceData.price;
+    }
+    if (otherServiceData.duration) {
+      existingService.duration = otherServiceData.duration;
+    }
+    if (otherServiceData.description) {
+      existingService.description = otherServiceData.description;
+    }
+    if (otherServiceData.description) {
+      existingService.description = otherServiceData.description;
+    }
+    if (imagesBuffers != null) {
+      const images = await imagesService.saveImageToFolderAndDatabase(
+        imagesBuffers,
+        file
+      );
+      existingService.images = images;
+    }
+
+    await existingService.save();
+    return existingService;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteById = async (serviceId) => {
+  try {
+    const deletedService = await Services.deleteOne({ _id: serviceId });
+
+    if (deletedService.deletedCount === 0) {
+      throw new Error("Service not found");
+    }
+    return { message: "Service deleted successfully" };
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { findAll, findById, create, updateById, deleteById };
