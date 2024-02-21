6c025e2721b4188b8ffedabdb5e6506a255b476b
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-21 10:10:54 +0300

    [FIX] appointment

---
+++ src/appointments/appointments.controller.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const AppointmentsServices = require('./credentials.service');
+const Appointments = require('./credentials.model');
+
+router.get("/",async (req, res) => {
+    try {
+      const AppointmentsData = await AppointmentsServices.findAll({});
+      res.status(200).json(AppointmentsData);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/:id",async (req, res) => {
+  try {
+    const AppointmentsData =  await AppointmentsServices.findById({ _id: req.params.id });
+    res.status(200).json(AppointmentsData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const AppointmentData = {
+        date: req.body.date,
+        client: req.body.client, 
+        state: req.body.state
+      };
+      const NewAppointments = await AppointmentsServices.create(AppointmentData);
+      res.status(200).send(NewAppointments);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+      const NewAppointments = await AppointmentsServices.updateById(req.params.id, req.body);
+      res.status(200).send(NewAppointments);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+      await AppointmentsServices.deleteById(req.params.id);
+      res.status(200).json({ message: 'Appointment deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
--- src/appointments/appointments.model.js
+++ src/appointments/appointments.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const appointmentsSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'clients', required: true }, 
+  state: { type: Number, required: true }
 });
 
 const Appointments = mongoose.model('appointments', appointmentsSchema);
