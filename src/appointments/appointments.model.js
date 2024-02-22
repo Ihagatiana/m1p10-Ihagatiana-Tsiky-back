362e9a1c29c3a92723225bd7884189332a71fd4e
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-22 15:18:15 +0300

    [Fix]  appointment

--- src/appointments/appointments.model.js
+++ src/appointments/appointments.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const appointmentsSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   clients: { type: mongoose.Schema.Types.ObjectId, ref: 'clients', required: true }, 
+  starttime: {
+    hours: { type: Number, required: true },
+    minutes: { type: Number, required: true }
+  },
   state: { type: Number, required: true }
 });
 
