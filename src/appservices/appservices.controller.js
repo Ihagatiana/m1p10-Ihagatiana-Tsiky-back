817f02e415b0321de13e34c589629f18b1f5f437
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-22 15:48:21 +0300

    [FIX] appointment

--- src/appservices/appservices.controller.js
+++ src/appservices/appservices.controller.js
@@ -26,13 +26,37 @@ router.get("/:id",async (req, res) => {
 });
 
 router.post("/",async (req, res) => {
-  const {} = req.body;
+
+  const appointmentData = {
+    date: req.body.date,
+    clients: req.body.clients,
+    startimes: req.body.startimes,
+    state : 1
+  }
+  const appServiceData = {
+    date: req.body.date,
+    clients: req.body.clients,
+    state : 1
+  }
+
   try {
+
     const appservices =  await appservicesService.findById({ _id: appservicesId });
     res.status(200).json(appservices);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
+
+/*
+    const otherServiceData = {
+      name: req.body.name,
+      price: req.body.price,
+      duration: req.body.duration,
+      comission:req.body.comission,
+      description: req.body.description,
+    };
+*/
 
 module.exports = router;
