55bc4ae3002753eec4b3f4d0962d51bb9e0004bc
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-26 21:27:08 +0300

    [FIX] images

--- src/services/services.collection.js
+++ src/services/services.collection.js
@@ -4,10 +4,10 @@
     "manicure":"65ccca852fc58b0ca2c14113"
     "pedicure":"65ccca852fc58b0ca2c15114"
     "maquillage":"65ccca852fc58b0ca2c11115"
-    "massage":"65ccca852fc58b0ca2c12116"
+    "soinvisage":"65ccca852fc58b0ca2c12116"
 */
 
-
+[
 {
     "_id": {
       "$oid": "65ccca852fc58b0ca2ce91e5"
@@ -15,12 +15,93 @@
     "name": "Massage",
     "price": 50000,
     "duration": {
-      "hours": 0,
-      "minutes": 10
+      "hours": 1,
+      "minutes": 0
     },
-    "description": " Ambiance Apaisante ,  Des Mains Expertes ",
+    "description": "Découvrez un oasis de relaxation avec nos massages personnalisés, où des praticiens expérimentés utilisent des techniques expertes pour apaiser vos tensions, rétablir l'équilibre énergétique, et vous offrir une évasion sensorielle inoubliable.",
     "comission": 10,
     "images": [
-      "65ccca852fc58b0ca2ce91e1"
+      "65ccca852fc58b0ca2c12111"
     ]
-}
+  },
+  {
+    "_id": {
+      "$oid": "65ccca852fc58b0ca2ce91e4"
+    },
+    "name": "Maquillage",
+    "price": 20000,
+    "duration": {
+      "hours": 0,
+      "minutes": 30
+    },
+    "description": "Sublimez votre beauté naturelle avec nos services de maquillage d'exception, où notre équipe de professionnels talentueux met en œuvre leur expertise pour créer des looks personnalisés qui révèlent votre éclat unique.",
+    "comission": 10,
+    "images": [
+      "65ccca852fc58b0ca2c11115"
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "65ccca852fc58b0ca2ce91e3"
+    },
+    "name": "Pedicure",
+    "price": 20000,
+    "duration": {
+      "hours": 0,
+      "minutes": 45
+    },
+    "description": "Sublimez votre beauté naturelle avec nos services de maquillage d'exception, où notre équipe de professionnels talentueux met en œuvre leur expertise pour créer des looks personnalisés qui révèlent votre éclat unique.",
+    "comission": 10,
+    "images": [
+      "65ccca852fc58b0ca2c15114"
+    ]
+   },
+   {
+    "_id": {
+      "$oid": "65ccca852fc58b0ca2ce91e2"
+    },
+    "name": "Manicure",
+    "price": 20000,
+    "duration": {
+      "hours": 0,
+      "minutes": 45
+    },
+    "description": "Offrez à vos mains une touche de luxe avec nos services de manucure raffinée, où la précision et l'attention aux détails se rencontrent pour vous offrir des ongles impeccables et une expérience de détente ultime.",
+    "comission": 10,
+    "images": [
+      "65ccca852fc58b0ca2c14113"
+    ]
+  },{
+    "_id": {
+      "$oid": "65ccca852fc58b0ca2ce91e1"
+    },
+    "name": "Coiffure",
+    "price": 40000,
+    "duration": {
+      "hours": 1,
+      "minutes": 15
+    },
+    "description": "Transformez votre style avec nos services de coiffure haut de gamme, où nos experts en coiffure allient créativité et expertise pour vous offrir des coupes, des couleurs et des styles personnalisés qui reflètent votre beauté individuelle.",
+    "comission": 10,
+    "images": [
+      "65ccca852fc58b0ca2c13112"
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "65ccca852fc58b0ca2ce91e0"
+    },
+    "name": "Soin visage",
+    "price": 40000,
+    "duration": {
+      "hours": 1,
+      "minutes": 0
+    },
+    "description": "Revitalisez votre peau et révélez votre éclat naturel avec nos soins du visage sur mesure, prodigués par nos esthéticiennes expérimentées. Plongez dans une expérience de détente et de bien-être, tandis que nous chouchoutons votre peau avec des produits de qualité et des techniques spécialisées.",
+    "comission": 10,
+    "images": [
+      "65ccca852fc58b0ca2c12116"
+    ]
+  }
+
+]
